55de30639761ac278fd69dbb02f25f4e7ae21693
Matthijs Bijman <matthijs@bijman.org>
2019-04-19 12:11:53 +0200

    Passing non reference vals

--- Compiler/include/fe/runtime/io.h
+++ Compiler/include/fe/runtime/io.h
@@ -7,14 +7,16 @@ namespace fe::stdlib::io
 	static module load()
 	{
 		using namespace types;
+		auto ui64t = ui64();
+		auto voidtt = voidt();
 		return module_builder()
 		  .set_name({ "std", "io" })
 		  .add_native_function(
 		    vm::PRINT, "print",
-		    make_unique(function_type(make_unique(ui64()), make_unique(voidt()))))
+		    make_unique(function_type(make_unique(ui64t), make_unique(voidtt))))
 		  .add_native_function(
 		    vm::PRINTLN, "println",
-		    make_unique(function_type(make_unique(ui64()), make_unique(voidt()))))
+		    make_unique(function_type(make_unique(ui64t), make_unique(voidtt))))
 		  .build();
 	}
 } // namespace fe::stdlib::io
